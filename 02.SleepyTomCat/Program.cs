3a1b9754619764ac970d1d23af5c819641a72b10
Zdravko Dimov <148683682+Zdrave88@users.noreply.github.com>
2024-09-19 12:48:28 +0300

    Add files via upload

---
+++ 02.SleepyTomCat/Program.cs
@@ -0,0 +1,27 @@
+﻿int freeDays = int.Parse(Console.ReadLine());
+int anualPlayTimeInMinutes = 30000;
+int year = 365;
+int minutesPerWorkingDay = 63;
+int minutesPerDaysOff = 127;
+
+// CALCULATIONS
+int workingDays = year - freeDays;
+int playPerWorkingDay = workingDays * minutesPerWorkingDay;
+int playPerDaysOff = freeDays * minutesPerDaysOff;
+int anualPlayTime = playPerWorkingDay + playPerDaysOff;
+int minutesToLimit = Math.Abs(anualPlayTimeInMinutes - anualPlayTime);
+
+int hours = minutesToLimit / 60;
+int minutes = minutesToLimit % 60;  
+
+
+if (anualPlayTime >= anualPlayTimeInMinutes)
+{
+    Console.WriteLine("Tom will run away");
+    Console.WriteLine($"{hours} hours and {minutes} minutes more for play");
+}
+else
+{
+    Console.WriteLine("Tom sleeps well");
+    Console.WriteLine($"{hours} hours and {minutes} minutes less for play"); 
+}
---
+++ 04.Harvest01/Program.cs
@@ -0,0 +1,39 @@
+﻿// INPUT
+int vineyardSquareMeters = int.Parse(Console.ReadLine());
+double grapePerSquareMeter = double.Parse(Console.ReadLine());  
+int desiredWinAmount  = int.Parse(Console.ReadLine());  
+int workers = int.Parse(Console.ReadLine());
+
+// CALCULATIONS
+double totalGrape = vineyardSquareMeters * grapePerSquareMeter;                 // смятам обема на лозето
+double wineLitter = (0.4 * totalGrape) / 2.5;                                   // смятам количеството вино в литри
+
+// OUTPUT
+if (wineLitter < desiredWinAmount)                                              // проверка за достатъчно вино
+{
+    double wineNeeded = Math.Floor(desiredWinAmount - wineLitter);
+    Console.WriteLine($"It will be a tough winter! More {wineNeeded} liters wine needed.");
+
+}
+else
+{
+    double wineSurplus = Math.Ceiling(wineLitter - desiredWinAmount);
+    double winePerWorker = Math.Ceiling(wineSurplus / workers);
+    Console.WriteLine($"Good harvest this year! Total wine: {wineSurplus} liters.");
+    Console.WriteLine($"{wineSurplus} liters left -> {winePerWorker} liters per person.");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Console.WriteLine(wineLitter);
