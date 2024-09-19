721e3f6f26824e3312552a7f8220e74201f36df8
Zdravko Dimov <148683682+Zdrave88@users.noreply.github.com>
2024-09-19 12:51:43 +0300

    Add files via upload

---
+++ Conditional Statements - More Exercises/03.Harvest/Program.cs
@@ -0,0 +1,21 @@
+﻿int cropArea = int.Parse(Console.ReadLine());
+double grapePerSquareMeter = double.Parse(Console.ReadLine());
+int desiredWine = int.Parse(Console.ReadLine());    
+int workers = int.Parse(Console.ReadLine());    
+//смятам обема на лозето
+double totalGrape = cropArea * grapePerSquareMeter;
+// смятам количеството вино в лоитри
+double wineLiters = (totalGrape * 0.4) / 2.5;
+// проверка за достатъчно вино
+if (wineLiters < desiredWine)
+{ 
+    double neededWine = Math.Floor(desiredWine - wineLiters);
+    Console.WriteLine($"It will be a tough winter! More {neededWine} liters wine needed.");
+}
+else
+{
+    double wineLeft = Math.Ceiling(wineLiters - desiredWine);
+    double winePerWorker = Math.Ceiling(wineLeft / workers);
+    Console.WriteLine($"Good harvest this year! Total wine: {wineLiters} liters.");
+    Console.WriteLine($"{wineLeft} liters left -> {winePerWorker} liters per person.");
+}
---
+++ Conditional Statements - More Exercises/06.FlowerShop/Program.cs
@@ -0,0 +1,26 @@
+﻿int magnols = int.Parse(Console.ReadLine());
+int zumbuls = int.Parse(Console.ReadLine());
+int roses = int.Parse(Console.ReadLine());
+int cactuses = int.Parse(Console.ReadLine());
+double presentPrice = double.Parse(Console.ReadLine());
+
+double magnolsPrice = 3.25;
+double zumbulsPrice = 4.00;
+double rosesPrice = 3.50;
+double cactusesPrice = 8.00;
+
+double moneyMade = (magnols*magnolsPrice) + (zumbuls*zumbulsPrice) + (roses*rosesPrice) + (cactuses*cactusesPrice);
+double taxes = (moneyMade * 5) / 100;
+
+double finalMoneyAmount = moneyMade - taxes;
+//double taxes2 = (moneyMade * 0.05);
+
+double difference = Math.Abs(finalMoneyAmount - presentPrice);
+if (finalMoneyAmount >= presentPrice)
+{
+    Console.WriteLine($"She is left with {Math.Floor(difference)} leva.");
+}
+else
+{
+    Console.WriteLine($"She will have to borrow {Math.Ceiling(difference)} leva.");
+}
